ebb8abb832482e969aff5b75b461258e1ae845db
Mark Wadham <m@rkw.me>
2014-05-29 07:41:26 +0000

    [ORB-197] bugfix

--- views/default/view_Element_OphCiPatientdischarge_DischargePrep.php
+++ views/default/view_Element_OphCiPatientdischarge_DischargePrep.php
@@ -40,7 +40,7 @@
 			<div class="large-3 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('change_noted'))?>:</div></div>
 			<div class="large-9 column end"><div class="data-value"><?php echo is_null($element->change_noted) ? 'Not recorded' : ($element->change_noted ? 'Yes' : 'No')?></div></div>
 		</div>
-		<?php if ($element->comments) {?>
+		<?php if ($element->change_noted && $element->comments) {?>
 			<div class="row data-row">
 				<div class="large-3 column"><div class="data-label"><?php echo CHtml::encode($element->getAttributeLabel('comments'))?></div></div>
 				<div class="large-9 column end"><div class="data-value"><?php echo $element->textWithLineBreaks('comments')?></div></div>
