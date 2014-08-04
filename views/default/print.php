23fea7219de318ad1b1dd6b2b357997e7e3de254
Richard Willis <willis.rh@gmail.com>
2014-08-04 11:46:54 +0100

    [ORB-323] Removed redundant print view files

--- views/default/print.php
+++ views/default/print.php
@@ -17,6 +17,7 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
 ?>
+
 <?php $this->renderPartial('//print/event', array(
 	'hide_modified' => @$hide_modified,
 	'hide_created' => @$hide_created
